2a226fc86b7b9d8f52c0debe678ff9bef1d25b87
LI1994 <Lyndee_irish@hotmail.com>
2017-12-05 19:11:28 -0700

    reverted code bas

--- server/server.js
+++ server/server.js
@@ -13,31 +13,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./config/error-handler'))
 
-const myFirstPost = {title: 'New Post', img: 'https://www.webwise.ie/wp-content/uploads/2014/04/Can-Instagram-Boost-Business-Sales.jpg' , caption : 'Mini Instagram'}
+'https://www.webwise.ie/wp-content/uploads/2014/04/Can-Instagram-Boost-Business-Sales.jpg' , caption : 'Mini Instagram'}
 
-app.post ('/api/posts', (req, res) => {
 
-  const {title, img, caption} = req.body
-  const newPost = {title, img, caption}
-
-  Post(newPost).save((err, savedPost) => {
-    if (err) {
-      res.json({ error: err })
-    } else {
-      res.json({ msg: 'Your post was successfully created', data: savedPost})
-    }
-  })
-})
-
-app.get('/api/posts', (req, res) => {
-  Post.find((err, posts) => {
-    if (err) {
-     res.json({ error: err })
-    } else {
-      res.json({ msg: 'SUCCESS', posts})
-    }
-  })
-})
 
 
 const server = app.listen(port, () => console.log(`🔥🔥🔥Running on port: ${port}`))
